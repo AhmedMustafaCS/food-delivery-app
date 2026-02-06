d7da641c9d6970b9fa5e49fdf573104064e36395
AhmedMustafa <ahmedmustafacs1@gmail.com>
2026-02-06 17:43:04 +0200

    Enhance the GridView item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey[100],
         appBar: AppBar(
           title: const Text('Foodak'),
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.grey[100],
           foregroundColor: Colors.black,
           elevation: 0,
           centerTitle: true,
@@ -30,63 +30,110 @@ class HomePage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            const SizedBox(height:32),
+            const SizedBox(height: 32),
             Expanded(
               child: GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing:20 ,
-                  crossAxisSpacing: 20,
-                ),
-                children:[
-                  Container(
-                    decoration:BoxDecoration(
-                      color:Colors.grey,
-                    ),
-                    child:Column(
-                      children:[
-                        Placeholder(
-                          fallbackHeight: 100,
-                        ),
-                        Text('Burger'),
-                        Text('25\$'),
-
-                      ]
-                      ),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20,
                   ),
-                  Container(
-                    decoration:BoxDecoration(
-                      color:Colors.grey,
-                    ),
-                    child:Column(
-                      children:[
-                        Placeholder(
-                          fallbackHeight:100,
-                        ),
-                        Text('Burger'),
-                        Text('25\$'),
-
-                      ]
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
                       ),
-                  ),
-                  Container(
-                    decoration:BoxDecoration(
-                      color:Colors.grey,
-                    ),
-                    child:Column(
-                      children:[
-                        Placeholder(
-                          fallbackHeight: 100,
-                        ),
-                        Text('Burger'),
-                        Text('25\$'),
-
-                      ]
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(children:const [
+                          Placeholder(
+                            fallbackHeight: 100,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Burger',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              )),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text('5 \$',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color:Colors.deepOrange,
+                        
+                              )),
+                        ]),
                       ),
-                  ),
-                 
-                ]
-              ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(children:const [
+                          Placeholder(
+                            fallbackHeight: 100,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Burger',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              )),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text('5 \$',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color:Colors.deepOrange,
+                        
+                              )),
+                        ]),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(children:const [
+                          Placeholder(
+                            fallbackHeight: 100,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Burger',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              )),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text('5 \$',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color:Colors.deepOrange,
+                        
+                              )),
+                        ]),
+                      ),
+                    ),
+                   
+                  ]),
             )
           ]),
         ));
