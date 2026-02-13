bef056dbd85ea3ab0adb92c6bd6993994cc40482
AhmedMustafa <ahmedmustafacs1@gmail.com>
2026-02-13 11:11:13 +0200

    Overlay toggleable favorite icon on the food image

--- lib/widgets/food_grid_item.dart
+++ lib/widgets/food_grid_item.dart
@@ -1,40 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/models/food_item.dart';
 
-class FoodGridItem extends StatelessWidget {
+class FoodGridItem extends StatefulWidget {
   final FoodItem foodItem;
   const FoodGridItem({super.key, required this.foodItem});
 
   @override
+  State<FoodGridItem> createState() => _FoodGridItemState();
+}
+
+class _FoodGridItemState extends State<FoodGridItem> {
+  bool isFav = false;
+  @override
   Widget build(BuildContext context) {
     return Container(
-      decoration:  BoxDecoration(
-        borderRadius:BorderRadius.circular(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.0),
         color: Colors.white,
       ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(children: [
-          Image.network(foodItem.imgUrl, height: 100, fit: BoxFit.cover),
+          Stack(
+            children: [
+              Image.network(
+                widget.foodItem.imgUrl,
+                height: 100,
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 27,
+                  width: 27,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: InkWell(
+                    onTap: () => setState(() {
+                      isFav = !isFav;
+                      
+                    }),
+                    child: Icon(
+                      isFav ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
           const SizedBox(
             height: 8,
           ),
-          Text(foodItem.name,
-              style: const TextStyle(
-                fontSize: 15.7,
-                fontWeight: FontWeight.w600,
-              ),
-              ),
+          Text(
+            widget.foodItem.name,
+            style: const TextStyle(
+              fontSize: 15.7,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
           const SizedBox(
             height: 4,
           ),
-          Text('\$ ${foodItem.price} ',
-              style: const TextStyle(
-                fontSize: 14.3,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
-              ),
-              ),
+          Text(
+            '\$ ${widget.foodItem.price} ',
+            style: const TextStyle(
+              fontSize: 14.3,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepOrange,
+            ),
+          ),
         ]),
       ),
     );
