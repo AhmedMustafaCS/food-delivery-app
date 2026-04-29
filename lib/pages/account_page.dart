3f2b3b2a5656c0b2813a550dbba59f2c2c3d3d14
AhmedMustafa <ahmedmustafacs1@gmail.com>
2026-04-29 17:34:51 +0200

    Adapt account page layout for iOS

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
@@ -28,19 +30,36 @@ class _AccountPageState extends State<AccountPage> {
   Widget itemTappedTile(BuildContext context, {required String title, String? subtitle, required IconData icon}) {
     final size = MediaQuery.sizeOf(context);
     final isLandscape = MediaQuery.orientationOf(context) == Orientation.landscape;
-    return ListTile(
-      title: Text(title),
-      leading: Icon(
-        icon,
-        size: isLandscape ? size.height * 0.09 : size.height * 0.033,
-      ),
-      onTap: () => debugPrint('$title clicked!'),
-      subtitle: subtitle != null ? Text(subtitle) : null,
-      trailing: Icon(
-        Icons.chevron_right,
-        size: isLandscape ? size.height * 0.09 : size.height * 0.03,
-      ),
-    );
+    return Platform.isAndroid
+        ? ListTile(
+            title: Text(title),
+            leading: Icon(
+              icon,
+              size: isLandscape ? size.height * 0.09 : size.height * 0.033,
+            ),
+            onTap: () => debugPrint('$title clicked!'),
+            subtitle: subtitle != null ? Text(subtitle) : null,
+            trailing: Icon(
+              Icons.chevron_right,
+              size: isLandscape ? size.height * 0.09 : size.height * 0.03,
+            ),
+          )
+        : CupertinoListTile(
+            padding: EdgeInsets.only(top: size.height * 0.02, bottom: size.height * 0.02, left: size.width * 0.04, right: size.width * 0.04),
+            title: Text(title),
+            leading: Icon(
+              color: Theme.of(context).primaryColor,
+              icon,
+              size: isLandscape ? size.height * 0.09 : size.height * 0.033,
+            ),
+            onTap: () => debugPrint('$title clicked!'),
+            subtitle: subtitle != null ? Text(subtitle) : null,
+            trailing: Icon(
+              color: Theme.of(context).primaryColor,
+              CupertinoIcons.chevron_right,
+              size: isLandscape ? size.height * 0.09 : size.height * 0.03,
+            ),
+          );
   }
 
   Widget personPhoto(double width, double height) => Container(
@@ -67,53 +86,110 @@ class _AccountPageState extends State<AccountPage> {
             fontWeight: FontWeight.w600,
           ),
     );
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          if (!isLandscape) ...[
-            personPhoto(size.width * 0.5, size.height * 0.25),
-            nameText,
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Platform.isAndroid
+        ? SingleChildScrollView(
+            child: Column(
               children: [
-                orderVoucherItem(context, name: 'Orders', number: 50),
-                orderVoucherItem(context, name: 'Vouchers', number: 10),
+                if (!isLandscape) ...[
+                  personPhoto(size.width * 0.5, size.height * 0.25),
+                  nameText,
+                  const SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      orderVoucherItem(context, name: 'Orders', number: 50),
+                      orderVoucherItem(context, name: 'Vouchers', number: 10),
+                    ],
+                  ),
+                  const SizedBox(height: 24.0),
+                ],
+                if (isLandscape) ...[
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                    Column(
+                      children: [
+                        personPhoto(size.width * 0.2, size.height * 0.45),
+                        const SizedBox(height: 8.0),
+                        nameText,
+                      ],
+                    ),
+                    Column(children: [
+                      orderVoucherItem(context, name: 'Orders', number: 50),
+                      const SizedBox(height: 16.0),
+                      orderVoucherItem(context, name: 'Vouchers', number: 10),
+                    ]),
+                  ]),
+                ],
+                const Divider(),
+                itemTappedTile(
+                  context,
+                  title: 'Past Orders',
+                  icon: Icons.shopping_cart,
+                ),
+                const Divider(),
+                itemTappedTile(
+                  context,
+                  title: 'Available Vouchers',
+                  icon: Icons.card_giftcard,
+                ),
+                const Divider(),
               ],
             ),
-            const SizedBox(height: 24.0),
-          ],
-          if (isLandscape) ...[
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              Column(
-                children: [
-                  personPhoto(size.width * 0.2, size.height * 0.45),
-                  const SizedBox(height: 8.0),
-                  nameText,
+          )
+        : SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 18.0),
+                if (!isLandscape)
+                  Column(
+                    children: [
+                      personPhoto(size.width * 0.5, size.height * 0.25),
+                      nameText,
+                      const SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          orderVoucherItem(context, name: 'Orders', number: 50),
+                          orderVoucherItem(context, name: 'Vouchers', number: 10),
+                        ],
+                      ),
+                      const SizedBox(height: 24.0),
+                    ],
+                  ),
+                if (isLandscape) ...[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          personPhoto(size.width * 0.2, size.height * 0.45),
+                          const SizedBox(height: 8.0),
+                          nameText,
+                        ],
+                      ),
+                      Column(children: [
+                        orderVoucherItem(context, name: 'Orders', number: 50),
+                        const SizedBox(height: 16.0),
+                        orderVoucherItem(context, name: 'Vouchers', number: 10),
+                      ]),
+                    ],
+                  ),
                 ],
-              ),
-              Column(children: [
-                orderVoucherItem(context, name: 'Orders', number: 50),
-                const SizedBox(height: 16.0),
-                orderVoucherItem(context, name: 'Vouchers', number: 10),
-              ]),
-            ]),
-          ],
-          const Divider(),
-          itemTappedTile(
-            context,
-            title: 'Past Orders',
-            icon: Icons.shopping_cart,
-          ),
-          const Divider(),
-          itemTappedTile(
-            context,
-            title: 'Available Vouchers',
-            icon: Icons.card_giftcard,
-          ),
-          const Divider(),
-        ],
-      ),
-    );
+                CupertinoListSection.insetGrouped(
+                  children: [
+                    itemTappedTile(
+                      context,
+                      title: 'Past Orders',
+                      icon: Platform.isAndroid ? Icons.shopping_cart : CupertinoIcons.shopping_cart,
+                    ),
+                    itemTappedTile(
+                      context,
+                      title: 'Available Vouchers',
+                      icon: Platform.isAndroid ? Icons.card_giftcard : CupertinoIcons.gift,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
   }
 }
