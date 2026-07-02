35808663b12d2cd3eb159d6bd438e5cd7dcf4828
AhmedMustafa <ahmedmustafacs1@gmail.com>
2026-07-02 13:59:53 +0200

    Refactor food details layout to make checkout section sticky

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -13,87 +13,103 @@ class FoodDetailsPage extends StatelessWidget {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
       //  appBar: AppBar(),
-      body: SingleChildScrollView(
+      body: SafeArea(
+        top: false,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TopBanner(foodIndex: foodIndex),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0, bottom: 46.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TopBanner(foodIndex: foodIndex),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0, bottom: 46.0),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    food[foodIndex].name,
+                                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                  const SizedBox(height: 6.0),
+                                  Text('Buffalo Burger', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.grey)),
+                                ],
+                              ),
+                              FoodItemCounter(),
+                            ],
+                          ),
+                          const SizedBox(height: 32.0),
+                          IntrinsicHeight(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                PropertyItem(propertyName: 'Size', propertyValue: 'Medium'),
+                                VerticalDivider(
+                                  indent: 0,
+                                  endIndent: 0,
+                                ),
+                                PropertyItem(propertyName: 'Calories', propertyValue: '150 Kcal'),
+                                VerticalDivider(
+                                  indent: 0,
+                                  endIndent: 0,
+                                ),
+                                PropertyItem(propertyName: 'Cooking', propertyValue: '10-20 Min'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
                           Text(
-                            food[foodIndex].name,
-                            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                                  fontWeight: FontWeight.w600,
+                            'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                  color: Colors.grey,
                                 ),
                           ),
-                          const SizedBox(height: 6.0),
-                          Text('Buffalo Burger', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.grey)),
+                          const SizedBox(height: 32.0),
                         ],
                       ),
-                      FoodItemCounter(),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+                left: 16.0,
+                right: 16.0,
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    '\$ ${food[foodIndex].price}',
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Theme.of(context).primaryColor, fontWeight: FontWeight.w600),
                   ),
-                  const SizedBox(height: 32.0),
-                  IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        PropertyItem(propertyName: 'Size', propertyValue: 'Medium'),
-                        VerticalDivider(
-                          indent: 0,
-                          endIndent: 0,
+                  const SizedBox(width: 46.0),
+                  Expanded(
+                    child: SizedBox(
+                      height: size.height * 0.055,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
                         ),
-                        PropertyItem(propertyName: 'Calories', propertyValue: '150 Kcal'),
-                        VerticalDivider(
-                          indent: 0,
-                          endIndent: 0,
+                        onPressed: () {},
+                        child: const Text(
+                          'Checkout',
+                          style: TextStyle(color: Colors.white),
                         ),
-                        PropertyItem(propertyName: 'Cooking', propertyValue: '10-20 Min'),
-                      ],
+                      ),
                     ),
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.grey,
-                        ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  Row(
-                    children: [
-                      Text(
-                        '\$ ${food[foodIndex].price}',
-                        style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Theme.of(context).primaryColor, fontWeight: FontWeight.w600),
-                      ),
-                      const SizedBox(width: 46.0),
-                      Expanded(
-                        child: SizedBox(
-                          height: size.height * 0.055,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              'Checkout',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
                 ],
               ),
             ),
