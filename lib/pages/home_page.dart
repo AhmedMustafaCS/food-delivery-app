400b0192e453280a8456b05a93cce8ee20741932
AhmedMustafa <ahmedmustafacs1@gmail.com>
2026-08-07 23:35:01 +0200

    Implement category selection state and food items filtering

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,6 +14,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? categoryChosenId;
+  late List<FoodItem> filteredFood;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    filteredFood = food;
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -34,40 +43,49 @@ class _HomePageState extends State<HomePage> {
             ),
             SizedBox(height: size.height * 0.03),
             SizedBox(
-              height:size.height*0.13,
+              height: size.height * 0.13,
               child: ListView.builder(
-                itemCount:categories.length,
-                scrollDirection:Axis.horizontal,
-                itemBuilder:(context,index)=>Padding(
-                  padding: const EdgeInsetsDirectional.only(end:16.0),
-                  child: Container(
-                    width:size.width*0.2,
-                  //  height: size.height*0.07,
-                    decoration:BoxDecoration(
-                      color:Colors.white,
-                      borderRadius: BorderRadius.circular(16.0)
-                    ),
-                    child:Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Image.asset(categories[index].imgPath),
-                          const SizedBox(height:8.0),
-                          Text(categories[index].title,style:Theme.of(context).textTheme.titleSmall),
-                        ],
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsetsDirectional.only(end: 16.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        categoryChosenId = categories[index].id;
+                      });
+                      filteredFood = filteredFood.where((item) => item.categoryId == categoryChosenId).toList();
+                    },
+                    child: Container(
+                      width: size.width * 0.2,
+                      //  height: size.height*0.07,
+                      decoration: BoxDecoration(
+                        color: categoryChosenId == categories[index].id ? Theme.of(context).primaryColor : Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Image.asset(categories[index].imgPath),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              categories[index].title,
+                              style: Theme.of(context).textTheme.titleSmall!.copyWith(color: categoryChosenId == categories[index].id ? Colors.white : null),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              
               ),
             ),
-            
             SizedBox(height: size.height * 0.03),
             GridView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
-              itemCount: food.length,
+              itemCount: filteredFood.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: isLandScape ? 4 : 2,
                 mainAxisSpacing: size.height * 0.015,
@@ -83,7 +101,10 @@ class _HomePageState extends State<HomePage> {
                     },
                   );
                 },
-                child: FoodGridItem(foodIndex: index),
+                child: FoodGridItem(
+                  foodIndex: index,
+                  filteredFood: filteredFood,
+                ),
               ),
             ),
           ],
